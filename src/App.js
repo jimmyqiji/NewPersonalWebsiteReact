c333d432c8b2ab54e035174f1df32f4f99f97a54
Jimmy Qi <jimmyqiji@hotmail.com>
2019-01-08 07:05:01 -0500

    hide element past about page now works even if you refresh anywhere

--- src/App.js
+++ src/App.js
@@ -16,12 +16,27 @@ class App extends React.Component {
 }
 
 $(document).ready( function() {
+  var nametop = document.getElementById("name").getBoundingClientRect().top;
+  var resumetop = document.getElementById("resume").getBoundingClientRect().top;
+  var portfoliotop = document.getElementById("portfolio").getBoundingClientRect().top;
+  var abouttop = $("#about").offset().top;
   $(window).scroll(function() {
-    var portfoliotop = $("#portfolio").offset().top;
-    var abouttop = $("#about").offset().top;
     console.log(abouttop);
     console.log(portfoliotop);
-    if(portfoliotop < abouttop) { //scrolled past the other div?
+    console.log($(window).scrollTop());
+    if($(window).scrollTop() + nametop < abouttop) { //scrolled past the other div?
+      $("#name").show(); //reached the desired point -- show div
+    }
+    else {
+      $("#name").hide();
+    }
+    if($(window).scrollTop() + resumetop < abouttop) { //scrolled past the other div?
+      $("#resume").show(); //reached the desired point -- show div
+    }
+    else {
+      $("#resume").hide();
+    }
+    if($(window).scrollTop() + portfoliotop < abouttop) { //scrolled past the other div?
       $("#portfolio").show(); //reached the desired point -- show div
     }
     else {
@@ -29,5 +44,26 @@ $(document).ready( function() {
     }
   });
 });
+
+// $(document).ready( function() {
+//   $(window).scroll(function() {
+//     hideElemScroll("#name", "#about");
+//     hideElemScroll("#resume", "#about");
+//     hideElemScroll("#portfolio", "#about");
+//   });
+// });
+
+// function hideElemScroll(s, p) {
+//   var sTop = $(s).offset().top;
+//   var pTop = $(p).offset().top;
+//   $(window).scroll(function() {
+//     if($(window).scrollTop() + sTop < pTop) { //scrolled past the other div?
+//       $(s).show(); //reached the desired point -- show div
+//     }
+//     else {
+//       $(s).hide();
+//     }
+//   });
+// }
 
 export default App;
