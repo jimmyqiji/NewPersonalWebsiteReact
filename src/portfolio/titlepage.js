6f20611d8946d58ee834dcc8ba71874d58b5ddaa
Jimmy <j35qi@uwaterloo.ca>
2019-01-13 21:14:45 -0500

    add routing from port to main

--- src/portfolio/titlepage.js
+++ src/portfolio/titlepage.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './titlepage.css'
+import { Link } from 'react-router-dom'
 
 class TitlePage extends React.Component {
 	render() {
 		return(
 			<div id="titlepage">
 				<div id="homebtn">
-					<button type="button" class="whitebtn btn btn-outline-secondary">Home</button>
+					<Link to="/">
+						<button type="button" class="whitebtn btn btn-outline-secondary">Home</button>
+					</Link>
 				</div>
 				<div id="title-text">
 					<div id="title"><h1>Portfolio</h1></div>
