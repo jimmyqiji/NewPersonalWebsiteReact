fcfaa10018131c4c10c4a32e831920d6d5a995b5
Jimmy Qi <jimmyqiji@hotmail.com>
2019-05-11 11:55:52 -0400

    set up slideshow for projects page

--- src/portfolio/projectpage.js
+++ src/portfolio/projectpage.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './projectpage.css'
+import Mappify from './projects/mappify'
 
 class ProjectPage extends React.Component {
 	render() {
 		return(
 			<div id="projectpage">
-
+				<div id="button_left"></div>
+				<div id="button_right"></div>
+				<div class="slideshow" cur_slide="0">
+					<div class="project" id="project0">
+						<Mappify />
+					</div>
+				</div>
 			</div>
 		);
 	}
