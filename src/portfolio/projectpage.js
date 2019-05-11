645be14b3269f7f5b886694e93292a27e9b21398
Jimmy Qi <jimmyqiji@hotmail.com>
2019-05-11 13:33:22 -0400

    project page class setup

--- src/portfolio/projectpage.js
+++ src/portfolio/projectpage.js
@@ -3,17 +3,51 @@ import './projectpage.css'
 import Mappify from './projects/mappify'
 
 class ProjectPage extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      projects: [<Mappify />]
+    };
+  }
+
+	renderdots() {
+		let dots = [];
+		for(let i = 0; i < this.state.projects.length; i++) {
+			dots.push(<PageDot value={i} />);
+		}
+		return dots;
+	}
+
+	renderProjects() {
+		let proj_list = [];
+		for(let i = 0; i < this.state.projects.length; i++) {
+			proj_list.push(
+				<div class="project" id={"project" + i}>
+					{this.state.projects[i]}
+				</div>
+			)
+		}
+		return proj_list;
+	}
+
 	render() {
 		return(
 			<div id="projectpage">
 				<div id="button_left"></div>
 				<div id="button_right"></div>
 				<div class="slideshow" cur_slide="0">
-					<div class="project" id="project0">
-						<Mappify />
-					</div>
+					{this.renderProjects()}
+					{this.renderdots()}
 				</div>
 			</div>
+		);
+	}
+}
+
+class PageDot extends React.Component {
+	render() {
+		return(
+			<div class="dot" id={"dot" + i}> </div>
 		);
 	}
 }
