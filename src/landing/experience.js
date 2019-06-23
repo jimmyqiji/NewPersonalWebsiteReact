5901185a30281fa6ee984c2ee4185aa7ec0f735e
Jimmy <jimmyqiji@hotmail.com>
2019-06-22 22:52:48 -0400

    Basic layout for work experience

--- src/landing/experience.js
+++ src/landing/experience.js
@@ -4,11 +4,28 @@ import './experience.css';
 class Experience extends React.Component {
 	render() {
 		return(
-            <div class="landing-foreground" id="landing-experience">
-                <h3 class="landing-section-title">Work Experience</h3>
+            <div id="landing-experience">
+                <div class="landing-foreground">
+                    <h3 class="landing-section-title">Work Experience</h3>
+                        <LondonLife />
+                </div>
             </div>
 		);
 	}
 }
 
+class LondonLife extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div id={this.props.id} class="landing-experience-block">
+                <h4 class="exp-job-title">Actuarial Student</h4>
+                <h5 class="exp-company">London Life Insurance Company</h5>
+            </div>
+        )
+    }
+}
 export default Experience;
