6f07333cdc6bf1037891126ce47008e118d97432
Jimmy <jimmyqiji@hotmail.com>
2019-06-24 00:34:08 -0400

    Work experience 1 done

--- src/landing/experience.js
+++ src/landing/experience.js
@@ -15,15 +15,23 @@ class Experience extends React.Component {
 }
 
 class LondonLife extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return(
-            <div id={this.props.id} class="landing-experience-block">
-                <h4 class="exp-job-title">Actuarial Student</h4>
-                <h5 class="exp-company">London Life Insurance Company</h5>
+            <div id="exp-londonlife" class="landing-experience-block">
+                <h4 class="exp-company">London Life Insurance Company</h4>
+                <div class="exp-company-body">
+                    <img class="exp-logo" src={require('../companies/londonlife.png')} />
+                    <div class="exp-company-description">
+                        <h5 class="exp-job-title">Actuarial Student</h5>
+                        <h6 class="exp-duration"><i>May 2018 - Aug 2018 London, ON.</i></h6>
+                        <div class="exp-details-list">
+                            <ul>
+                                <li>London Life uses models to predict how many people would claim their life insurance benefit each month (ie. die), so that they could react in accordance. I conducted analysis on the results from the model to figure out what amount the company should keep in the reserve and which portions can be re-invested.</li>
+                                <li>As a final project, I investigated the impacts of the change from using a log-normal model to using a stochastic volatility model to predict CIA regulations (on minimum reserve values) and presented my results to the entire company.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
