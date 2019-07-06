8433503afaa65e7ce9354ac7ecae0d23da6bb879
Jimmy <jimmyqiji@hotmail.com>
2019-07-06 15:40:55 -0400

    navbar aethetics

--- src/landing/navbar.js
+++ src/landing/navbar.js
@@ -8,16 +8,19 @@ class Navbar extends React.Component {
                 <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <p>Home</p>
+                            <span />
+                        </li>
+                        <li class="nav-item"><p>About Me</p></li>
+                        <li class="nav-item"><p>Experience</p></li>
+                    </ul>
+                </div>
                 <a class="navbar-brand" href="#">
                     <img src={require('../resources/home.png')} />
                 </a>
-                <div class="collapse navbar-collapse">
-                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item">Home</li>
-                        <li class="nav-item">About Me</li>
-                        <li class="nav-item">Experience</li>
-                    </ul>
-                </div>
             </nav>
 		);
 	}
