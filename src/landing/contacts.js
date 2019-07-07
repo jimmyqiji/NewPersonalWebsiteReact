97c22cde24c493913e742918e048becffb674f0c
Jimmy <jimmyqiji@hotmail.com>
2019-07-07 13:44:13 -0400

    colour changes

--- src/landing/contacts.js
+++ src/landing/contacts.js
@@ -6,12 +6,16 @@ class Contacts extends React.Component {
 		return(
 			<div id="contacts-page" class="landing-foreground">
                 <div id="contacts-title" class="landing-section-title">I can help!</div>
+                <span class="section-description">Do you have a <span class="fancy-font">funky</span> idea for a project? Or simply fancy a chat?</span>
                 <div id="send-email-btn" class="col-md-12">
                     <a href="mailto:j35qi@edu.uwaterloo.ca" class="btn-svg fadein">
                         <svg>
                             <rect x="2" y="2" rx="0" fill="none" width="266" height="50"></rect>
                         </svg>
-                        <span>Send me an email!</span>
+                        <span>
+                            <i class="fas fa-envelope"></i>
+                            Shoot me
+                        </span>
                     </a>
                 </div>
 			</div>
