c3a1171f6079fa65d332ef417bd7c54acd4af1b2
Jimmy <jimmyqiji@hotmail.com>
2019-07-07 21:36:33 -0400

    Finish writeup

--- src/landing/aboutpage.js
+++ src/landing/aboutpage.js
@@ -12,9 +12,10 @@ class AboutPage extends React.Component {
 						<h1 id="about-title">Who am I?</h1>
 						<div id="portrait"></div>
 						<div id="about-description">
-							<p>I came to Canada when I was grade 5, since then, I've lived in Vancouver. Growing up there, I had lots and lots of salmon and I absolutely loved it.</p>
-							<p>As you might have guessed, I'm religious when it comes to food. Aside from salmon, I also love eating hotpot, sushi, noodles and bbq ribs (I really enjoy a good can of beer as well). As a hobby, I go on food advantures every week, it's not just to give myself a chance to eat out, I just get a sense of accomplishment when I explore different restaurants.</p>
-							<p>Currently, I'm a third year in the University of Waterloo pursuing a double major in Computer Science and Pure Mathematics. I am personally really interested in mathematical understandings that can help build the foundations of some other physical realizations. For example I was really intrigued when I heard that spacetime was modelled by a 4 dimensional Lorentzian manifold (Einstein).</p>
+							<p>Currently, a third year in the University of Waterloo pursuing a double major in Computer Science and Pure Mathematics. I am really interested in computer vision, graphics design, as well as mathematical understandings that can help build the foundations of other more practical realizations.</p>
+							<p>I came to Canada when I was grade 5, since then, I've lived in Vancouver. Growing up there had me falling in love with eating seafood, especially salmon.</p>
+							<p>As you might have guessed, I'm religious when it comes to food. Aside from seafood, I also love eating hotpot, ramen, bbq ribs (and beer as well). As a hobby, I go on food advantures every week, having coop and going to different cities really helps broaden the menu. It is, hands down, the best part about having coop.</p>
+							<p>All these foods calls for lots of exercize as well, I enjoy working out, playing ball, swimming, and rock climbing. So far, it's been a great balance for me, I somehow managed to stay both somewhat lean, and at the same time, well fed. </p>
 						</div>
 					</div>
 				</div>
