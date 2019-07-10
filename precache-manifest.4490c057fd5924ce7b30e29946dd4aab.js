1f142d4b655b04a03b400222b23f94ae99f74fc7
Jimmy <jimmyqiji@hotmail.com>
2019-07-09 20:13:03 -0400

    Updates

---
+++ precache-manifest.4490c057fd5924ce7b30e29946dd4aab.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "70af30682309d76b0779db29a9024ba5",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax1.70af3068.png"
+  },
+  {
+    "revision": "d57cc18374aed003ee19",
+    "url": "/NewPersonalWebsiteReact/static/css/main.b02dd822.chunk.css"
+  },
+  {
+    "revision": "9f7661a7e4a059001886962d0b261b1c",
+    "url": "/NewPersonalWebsiteReact/static/media/vim.9f7661a7.jpg"
+  },
+  {
+    "revision": "6d29b59d8700ca64d204",
+    "url": "/NewPersonalWebsiteReact/static/js/1.6d29b59d.chunk.js"
+  },
+  {
+    "revision": "c2a7974fd688ec5829d3",
+    "url": "/NewPersonalWebsiteReact/static/js/runtime~main.c2a7974f.js"
+  },
+  {
+    "revision": "d0e5dd63ff6b5240f6af5513cdeaf389",
+    "url": "/NewPersonalWebsiteReact/static/media/brush.d0e5dd63.woff"
+  },
+  {
+    "revision": "cd673bcf9dc894d4a9bb939d25e12407",
+    "url": "/NewPersonalWebsiteReact/static/media/brush.cd673bcf.eot"
+  },
+  {
+    "revision": "68fdf7b60c4e8ea056997183efe67cd1",
+    "url": "/NewPersonalWebsiteReact/static/media/brush.68fdf7b6.ttf"
+  },
+  {
+    "revision": "e8d18d487cb07e3d2c25a4c27706386a",
+    "url": "/NewPersonalWebsiteReact/static/media/brush.e8d18d48.svg"
+  },
+  {
+    "revision": "4d2ee0f93b05e1fdb8f8363512dac9c1",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax2.4d2ee0f9.png"
+  },
+  {
+    "revision": "5d20159de8bd3b278360be7fdadcb4ff",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax7.5d20159d.png"
+  },
+  {
+    "revision": "3863a9a7955444e2b735f31ca1bf5214",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax4.3863a9a7.png"
+  },
+  {
+    "revision": "630f81ffdc512dcd60fdf1d4fc9e383e",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax6.630f81ff.png"
+  },
+  {
+    "revision": "d57cc18374aed003ee19",
+    "url": "/NewPersonalWebsiteReact/static/js/main.d57cc183.chunk.js"
+  },
+  {
+    "revision": "b555e24e2e300370ba0b8367f6ddc6f8",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax5.b555e24e.png"
+  },
+  {
+    "revision": "33cc3e3619e86d536bbcb7965c5684c4",
+    "url": "/NewPersonalWebsiteReact/static/media/keyart-mobile.33cc3e36.jpg"
+  },
+  {
+    "revision": "6b3efa1f3146bb8e0d5324692e12860d",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax0.6b3efa1f.png"
+  },
+  {
+    "revision": "3141a1e293960b4c83ecd2bc646138a4",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax3.3141a1e2.png"
+  },
+  {
+    "revision": "b35b5de97382560a5573dd18b253bf1a",
+    "url": "/NewPersonalWebsiteReact/static/media/parallax8.b35b5de9.png"
+  },
+  {
+    "revision": "54468fd19e8eaea18471f315da3cccfd",
+    "url": "/NewPersonalWebsiteReact/static/media/resume.54468fd1.pdf"
+  },
+  {
+    "revision": "10c46e81beb53ae4c5ad10b5e34ea657",
+    "url": "/NewPersonalWebsiteReact/static/media/portrait.10c46e81.png"
+  },
+  {
+    "revision": "43c4ea313b0b5b342a8526dd87e3cee2",
+    "url": "/NewPersonalWebsiteReact/static/media/hubdoc.43c4ea31.png"
+  },
+  {
+    "revision": "11fae3934872368df7b5ad4fb4f6e4b2",
+    "url": "/NewPersonalWebsiteReact/static/media/londonlife.11fae393.png"
+  },
+  {
+    "revision": "efe4ff28d6f5141a9c8cd737c2ef5867",
+    "url": "/NewPersonalWebsiteReact/static/media/port-landing.efe4ff28.jpg"
+  },
+  {
+    "revision": "e1a65fe68309ae527f542fb28c875dfb",
+    "url": "/NewPersonalWebsiteReact/static/media/mappify.e1a65fe6.jpg"
+  },
+  {
+    "revision": "6d29b59d8700ca64d204",
+    "url": "/NewPersonalWebsiteReact/static/css/1.64a19c55.chunk.css"
+  },
+  {
+    "revision": "6610d9727fb4b57e3a75adc5742f82cf",
+    "url": "/NewPersonalWebsiteReact/index.html"
+  }
+];
