73a0c672cbe27c30a9af751be3e3d5971c192e67
Jimmy <jimmyqiji@hotmail.com>
2019-07-10 18:47:46 -0400

    change some selectors

--- src/portfolio/titlepage.js
+++ src/portfolio/titlepage.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom'
 class TitlePage extends React.Component {
 	render() {
 		return(
-			<div id="titlepage">
+			<div id="port-title-page">
 				<div id="homebtn">
 					<Link to="/">
 						<button type="button" class="whitebtn btn btn-outline-secondary">Home</button>
 					</Link>
 				</div>
-				<div id="title-text">
-					<div id="title"><h1>Portfolio</h1></div>
-					<div id="subtitle"><h2>A Collection of Projects</h2></div>
+				<div id="port-title-text">
+					<div><h1>Portfolio</h1></div>
+					<div><h2>A Collection of Projects</h2></div>
 				</div>
-				<div id="scrldown1" class="scrldown clearfix">
+				<div id="port-scrldown1" class="port-scrldown clearfix">
 					<a scrlto="#projectpage"><span></span>Scroll</a>
 				</div>
 			</div>
